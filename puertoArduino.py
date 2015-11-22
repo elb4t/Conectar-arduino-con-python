b316d7ad01470d0999849479a9abc2eb89b048d6
elb4t <eloy4t@gmail.com>
2015-11-22 19:07:12 +0100

    Conexión con arduino
    Modulo para buscar automáticamente el puerto al que esta conectado la
    placa arduino en Linux

---
+++ puertoArduino.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+####################################
+#            Autor: elb4t          #
+####################################
+
+# Este método búsca automaticamente el puerto de arduino
+# y devuelve TRUE si lo a encontrado y FALSO cuando no lo encuentra
+
+def buscaPuerto():
+    # Varialbe para saber si hemos encontrado el puerto o no
+    bEncontrado = False
+    # Hacemos un bulce para recorrer los puertos que queremos comprobar
+    for iPuerto in range(0, 4):
+        try:
+            # Puerto que vamos a probar
+            PUERTO = '/dev/ttyUSB' + str(iPuerto)
+            # Velocidad 
+            VELOCIDAD = '9600'
+            # Probamos ha abrir el puerto
+            Arduino = serial.Serial(PUERTO, VELOCIDAD)
+            # si no se ha producido un error, cerramos el puerto
+            Arduino.close()
+            # cambiamos el estado del la variable para saber si lo hemos encontrado
+            bEncontrado = True
+            # Salimos del bucle
+            break
+        except:
+            # Si hay error, no hacemos nada y continuamos con la busqueda
+            pass
+    return bEncontrado
+pass
